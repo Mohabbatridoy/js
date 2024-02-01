06ad55fc6169a65c1017a9549b73443268a6b33b
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-02-01 09:21:41 +0600

    Practice Regular Expresion

--- Script.js
+++ Script.js
@@ -1,10 +1,34 @@
-//error Handling:
+//Regular expresion : 
+//Pattern Matching : 
 
-let a = prompt("Enter a number: ");
-try {
-    if(a>15) throw  "too big"
-    else if (a<5) throw "too small"
-} 
-catch (err){
-    console.log(err)
-}
+let re;
+let str;
+
+re = /hello/i; // case insensitive 
+
+console.log(re)
+console.log(re.source)
+
+str = 'hello world'
+str = "again hello world"
+str = "lskdjfrrhellomnks kshfr"
+
+//exec() - returns rsult in an array or null
+let result = re.exec(str)
+
+// test() - true/ false
+result = re.test(str)
+
+//match() - Returns array or null: 
+result = str.match(re)
+
+//search() - Returns index of the first match or -1
+result = str.search(re)
+
+//replace() - Return new string
+str = "Again Hello world"
+let new_str = str.replace(re,'Hi')
+
+console.log(new_str)
+
+console.log(result)
