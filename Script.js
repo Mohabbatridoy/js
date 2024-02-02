4f780458aaabdc96247c2eb18fe77d99ebd990f8
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-02-02 12:01:30 +0600

    practice re

--- Script.js
+++ Script.js
@@ -1,35 +1,48 @@
-let re;
-let str;
+//character set using Brackets []:
 
-//Literal character: 
-re = /emnei/i;
-str = "hello world"
+let re = /h[ea]llo/i;  //must be one of them inside brackets.
+re = /[^ha]ello/i; //Anything except those inside bracket
+re = /^[ha]ello/i; //must start with h or a 
+re = /[a-z]ello/i; // Start with the range mentioned in [] this brackets
+re = /^[A-Z]/i; // must be the fist letter in uper case.
+re = /[A-Za-z]ello/ ; // start with any character dosen't matter.
+re = /[0-9]ello/;
+re = /[^0-9]ello/;
 
-//Meta characters;
-re = /^hello/i ; // Must start with the caret symbol 
-re = /hello$/i ; // must end with dollar sign word.
-re = /^hello$/i; //must start with hello word for caret sign and must end with hello word for dolar sign.
-re = /^h.llo$/i ; //match any character one positon of dot.
-re = /h*llo/; // 0 or more character can have there.
-re = /he?llo/
-re = /he?a?llo/i; // optional character
-re = /hello\?/i;
+///Braces {}  - Quantifier: 
+re = /el{2,6}o/;
 
-str = "hello";
-str = "hyllo";
-str = "hksdhfjshfhsllo";
-str = "hello"
-str = "hllo"
-str = "hello\?"
+//Parentheses  () -- grouping
+re = /^([0-9]){10}/;
+re = /^01[0-9]{9}$/;
+re = /^\+8801[0-9]{9}$/;
+re = /([0-9]x){3}/;
 
-console.log(re.exec(str));
+let str = "hello"
+str = "ello"
+str = "mqkello"
+str = "Mohabbat"
+str = "MOhabbatello"
+str = "45ello"
+str = "Again 4ello"
+str = "23hsalffs"
+str = "sadfh"
+str = "1helo"
+str = "hellllllllo"
+str = "123456789010maoshfsoh"
+str = "01997058623"
+str = "+8801997058623"
+str = "2x3x4x"
+
+
+console.log(re.test(str))
 reTest(re,str)
 
-function reTest(r,s){
-    if(re.test(str)){
-        console.log(`${str} matches ${re.source}`)
+function reTest(re,str){
+    if (re.test(str)){
+        console.log(`${str} matches with ${re.source}`)
     }
     else{
-        console.log(`${str} dosen't matches ${re.source}`)
+        console.log(`${str} dosen't matches with ${re.source}`)
     }
 }
