9185863fc4c7b0fed93bb9f59487c6df9099de34
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-02-04 23:15:36 +0600

    practice ajax :

--- Script.js
+++ Script.js
@@ -10,6 +10,10 @@ function loadData(){
     //create xmlhttprequest class's object: 
     let xhr = new XMLHttpRequest();
 
+    xhr.onprogress = function(){
+        console.log(xhr.readyState);
+    }
+
     //load data by open class: 
     xhr.open('GET','data.txt',true);
 
@@ -21,10 +25,24 @@ function loadData(){
 
         if (this.status === 200){
             //console.log(this.status)
-            document.getElementById('output').innerHTML = `<h4>${this.responseText}</h4?`
+            document.getElementById('output').innerHTML = `<h4>${this.responseText}</h4>`
         }
 
     }
+    // xhr.onreadystatechange = function(){
+    //        //     // readyState Values
+    // //     // 0: request not initialized
+    // //     // 1: server connection established
+    // //     // 2: request received
+    // //     // 3: processing request
+    // //     // 4: request finished and response is ready
+    // console.log(this.readyState)
+    // if (this.status === 200 && this.readyState === 4){
+    //     console.log(this.responseText)
+    // }
+
+    // }
+
     xhr.send();
     // console.log(xhr)
 }
