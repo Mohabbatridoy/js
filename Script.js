20ddc000dfcdce4458944147c7c0eb3f899b9369
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-02-04 23:17:10 +0600

    practice ajax again

--- Script.js
+++ Script.js
@@ -29,19 +29,19 @@ function loadData(){
         }
 
     }
-    // xhr.onreadystatechange = function(){
-    //        //     // readyState Values
-    // //     // 0: request not initialized
-    // //     // 1: server connection established
-    // //     // 2: request received
-    // //     // 3: processing request
-    // //     // 4: request finished and response is ready
-    // console.log(this.readyState)
-    // if (this.status === 200 && this.readyState === 4){
-    //     console.log(this.responseText)
-    // }
+    xhr.onreadystatechange = function(){
+           //     // readyState Values
+    //     // 0: request not initialized
+    //     // 1: server connection established
+    //     // 2: request received
+    //     // 3: processing request
+    //     // 4: request finished and response is ready
+    console.log(this.readyState)
+    if (this.status === 200 && this.readyState === 4){
+        console.log(this.responseText)
+    }
 
-    // }
+    }
 
     xhr.send();
     // console.log(xhr)
