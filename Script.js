96d7b9e91ece1f88f63f73c1491c2bdaa03a9d2b
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-02-08 11:05:43 +0600

    practice fetch api

--- Script.js
+++ Script.js
@@ -1,33 +1,11 @@
-//promises: 
+//Fetch API
+//Fetch API uses Javascript Promise
 
-let persons = [
-    {firstName: "Mohabbt", LastName: "Hossain"},
-    {firstName: "Rezwan", LastName: "Haque"}
-];
+document.getElementById("get_data").addEventListener('click', getData);
 
-function createPerson(person){
-    let prom = new Promise(function(resolve,reject){
-        persons.push(person);
-        let error = false;
-        if (error!=true){
-            resolve();
-        }
-        else{
-            reject(`error somthing wrong`);
-        }
-    })
-    return prom;
+function getData(){
+    fetch('https://api.api-ninjas.com/v1/jokes?')
+    .then(res => res.json())
+    .then((Data)=> console.log(Data))
+    .catch((err) => console.log(err))
 }
-
-function getPerson(){
-    setTimeout(() => {
-        let output = '';
-        persons.forEach( function(person) {
-            output += `<li> ${person.firstName} ${person.LastName}</li>`;
-        }), document.getElementById('output').innerHTML = output;
-    }, 500);
-}
-
-createPerson({firstName: "shahed", LastName: "Mitul"}).then(getPerson).catch(function(err){
-    console.log(err);
-});
